768e46451d7659c6d81d089b1767d8e71048ac1f
Dang Nguyen <117255808+g-n-a-d@users.noreply.github.com>
2022-11-27 17:23:48 +0700

    fix

--- train.py
+++ train.py
@@ -6,8 +6,9 @@ def train(model, data_loader, kp_detector, loss_function, num_epochs=10, lr=1e-4
     scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=milestones_lr, gamma=gamma)
     model.train()
     if log:
-        log_loss = []
+        f = open('log/log_loss_{}.txt'.format(type(model).__name__), 'a')
     for epoch in range(num_epochs):
+        log_loss = []
         for i, (s, d) in enumerate(data_loader):
             pred = model(s, d)
             loss = criterion(pred['frame_generated'], d, kp_detector, pred['kp_driving'])
@@ -16,11 +17,10 @@ def train(model, data_loader, kp_detector, loss_function, num_epochs=10, lr=1e-4
             optimizer.step()
             if display:
                 print('epoch {}/{}: [{}/{}] -------> loss: {}'.format(epoch + 1, num_epochs, i + 1, len(data_loader), loss.item()))
+            log_loss.append(loss.item())
         scheduler.step()
         if log:
-            log_loss.append(loss.item())
-    if log:
-        with open('log/log_loss_{}.txt'.format(type(model).__name__), 'a') as f:
             for i in log_loss:
                 f.write(str(i) + ' ')
-            f.write('\n')
+        f.write('\n')
+    f.close()
