76bd14c6c3b8dadb4d7614afcd60c8317eca44c2
Dang Nguyen <117255808+g-n-a-d@users.noreply.github.com>
2022-12-11 12:11:31 +0700

    Update train.py

--- train.py
+++ train.py
@@ -5,6 +5,7 @@ def train(model, data_loader, loss_function, checkpoint_epoch=0, num_epochs=10, 
     criterion = loss_function
     optimizer = torch.optim.Adam(params=model.parameters(), lr=lr)
     scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=milestones_lr, gamma=gamma)
+    scheduler.step(checkpoint_epoch + 1)
     model.train()
     for epoch in range(checkpoint_epoch, num_epochs):
         log_loss = {}
